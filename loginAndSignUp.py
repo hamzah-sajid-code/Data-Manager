dfc9d0f2d1554e170e9e1e78e5e38712f98d40ce
hamzah-sajid-code <hamzahsajid10@gmail.com>
2022-06-24 09:22:16 +0300

    Completed login and signup features in gui

--- loginAndSignUp.py
+++ loginAndSignUp.py
@@ -1,14 +1,11 @@
 # Importing modules
-import os
 import json
-import time
-import main_functions
-import tkinter as tk
-from tkinter import filedialog
-from tkinter import messagebox
-from tkinter import ttk
-from tkinter import *
 import os
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+
+import main_functions
 
 
 class EntryWithPlaceholder(tk.Entry):
@@ -37,6 +34,7 @@ class EntryWithPlaceholder(tk.Entry):
         if not self.get():
             self.put_placeholder()
 
+
 root = Tk()
 
 color = "#CAF1DE"
@@ -46,15 +44,16 @@ root.geometry("600x500")
 root.configure(bg="#CAF1DE")
 
 userEntryMode = "login"
+usernameData = ""
 
 label_Title = Label(root, text="Data Manager", bg=color,
                     font=("@Yu Gothic UI Semibold", 20, "normal"))
-label_Title.place(relx=0.5,rely=0.05,anchor=CENTER)
+label_Title.place(relx=0.5, rely=0.05, anchor=CENTER)
 
 input_Username = EntryWithPlaceholder(root, placeholder="Username")
 input_Username["width"] = 40
 input_Username["font"] = ("@Yu Gothic UI Semibold", 15, "normal")
-input_Username.place(relx=0.5,rely=0.2, anchor=CENTER)
+input_Username.place(relx=0.5, rely=0.2, anchor=CENTER)
 
 input_Password = EntryWithPlaceholder(root, placeholder="Password")
 input_Password["width"] = 40
@@ -62,17 +61,19 @@ input_Password["font"] = ("@Yu Gothic UI Semibold", 15, "normal")
 input_Password.place(relx=0.5, rely=0.3, anchor=CENTER)
 
 button_Login = Button(root, text="Log In", relief="flat",
-                      padx=70, pady=5, bg="#2C95F6", fg="white",font=("Arial", 10,"bold"))
-button_Login.place(relx=0.5,rely=0.45, anchor=CENTER)
+                      padx=70, pady=5, bg="#2C95F6", fg="white", font=("Arial", 10, "bold"))
+button_Login.place(relx=0.5, rely=0.45, anchor=CENTER)
 
 label_DontHaveAAccount = Label(root, text="Don't have an account? ", font=(
     "Arial", 13, "normal"), bg=color)
-label_DontHaveAAccount.place(relx=0.3,rely=0.7)
+label_DontHaveAAccount.place(relx=0.3, rely=0.7)
 
-button_DontHaveAAccount = Button(root, text="Sign Up", relief="flat", bg=color, font=("Arial", 13, "normal"), fg="#2C95F6",)
+button_DontHaveAAccount = Button(root, text="Sign Up", relief="flat", bg=color, font=("Arial", 13, "normal"),
+                                 fg="#2C95F6", )
 
-label_msg = Label(root,text="",fg="red",bg=color, font=("Arial", 13, "bold"))
-label_msg.place(relx=0.5,rely=0.6, anchor=CENTER)
+label_msg = Label(root, text="", fg="red", bg=color, font=("Arial", 13, "bold"))
+label_msg.place(relx=0.5, rely=0.6, anchor=CENTER)
+
 
 def changeUserEntryMode():
     global userEntryMode
@@ -89,8 +90,11 @@ def changeUserEntryMode():
         button_Login["text"] = "Log In"
         button_DontHaveAAccount["text"] = "Sign Up"
         label_DontHaveAAccount["text"] = "Don't have an account? "
+
+
 button_DontHaveAAccount["command"] = changeUserEntryMode
-button_DontHaveAAccount.place(relx=0.59, rely=0.69,)
+button_DontHaveAAccount.place(relx=0.59, rely=0.69, )
+
 
 def handle():
     if userEntryMode == "login":
@@ -98,8 +102,11 @@ def handle():
     elif userEntryMode == "signup":
         signup()
 
+
 with open('accounts.json') as json_file:
     accountsData = json.load(json_file)
+
+
 def login():
     global usernameData
     global label_msg
@@ -125,54 +132,35 @@ def login():
                              "Username does not exists! Please try again.")
         return username
 
+
 button_Login["command"] = handle
 
+
 def signup():
-    pass
-
-# usernameData = ""
-# runningFunction = "handle"
-# def signup(username,password):
-#     global runningFunction
-#     runningFunction = "signup"
-#     if username != "" and password != "":
-#         if username in accountsData["accounts"]:
-#             print("Username already exists. Please try again.")
-#             signup()
-#         encryptedPassword = main_functions.encryptText(username, password)
-#         accountsData["accounts"][username] = encryptedPassword
-#         with open('accounts.json', 'w') as outfile:
-#             json.dump(accountsData, outfile)
-#         os.system("cls")
-#         os.mkdir(str(os.getcwd())+"\\Data\\"+username)
-#         with open(str(os.getcwd())+"\\Data\\"+username+"\\"+"data.json", "w") as f:
-#             f.write("{\"data\": {}}")
-#         print("\n")
-#         username = usernameData
-
-
-# def login(username,password):
-#     global runningFunction
-#     runningFunction = "login"
-#     global usernameData
-#     usernameData = username
-#     if username in accountsData["accounts"]:
-#         encryptedPassword = accountsData["accounts"][username]
-#         decryptedPassword = main_functions.decryptData(username, encryptedPassword)
-#         if password == decryptedPassword:
-#             print("Login successful.")
-#             print("\n")
-#             os.system("cls")
-#             os.system(f"py handler.py \"{username}\"")
-#         else:
-#             print("Incorrect password. Please try again.")
-#             login()
-#             print("\n")
-#     else:
-#         print("Username does not exist. Please try again.")
-#         login()
-#         print("\n")
-#         return username
+    global label_msg
+    global usernameData
+    username = input_Username.get()
+    password = input_Password.get()
+    usernameData = username
+    if username != "" and password != "":
+        if username in accountsData["accounts"]:
+            label_msg["fg"] = "red"
+            label_msg["text"] = "Username already exists. Please try again."
+        else:
+            encryptedPassword = main_functions.encryptText(username, password)
+            accountsData["accounts"][username] = encryptedPassword
+            with open('accounts.json', 'w') as outfile:
+                json.dump(accountsData, outfile)
+            os.system("cls")
+            os.system(f'mkdir \"{str(os.getcwd())}"\\Data\\"{username}\"')
+            with open(str(os.getcwd()) + "\\Data\\" + username + "\\" + "data.json", "w") as f:
+                f.write("{\"data\": {}}")
+            username = usernameData
+            label_msg["fg"] = "lime"
+            label_msg["text"] = "Account Successful created"
+            messagebox.showinfo("Account created Successful",
+                                "Your account is successfully make now we're logging you in")
+            login()
 
 
 root.mainloop()
